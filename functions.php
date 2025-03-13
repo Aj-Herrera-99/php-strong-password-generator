bc32747983f3d0c37edf6394338b154b1ed2ed9f
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-03-13 23:03:33 +0100

    ML4: generazione pswd senza ripetizioni. Semplificazione logica generazione random pswd

--- functions.php
+++ functions.php
@@ -1,27 +1,39 @@
 <?php
-function generatePassword($length): string
+function generatePassword(int $length, bool $with_repetition): string
 {
     if ($length <= 0) return "Nessun parametro valido inserito";
+
+    // possibili caratteri di una password
     $lc_letters = 'abcdefghijklmnopqrstuvwxyz';
     $uc_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $numbers = '0123456789';
     $symbols = '!$%&?@#+';
 
-    $pswd_chars = [
-        $lc_letters,
-        $uc_letters,
-        $numbers,
-        $symbols
-    ];
+    $pswd_chars =
+        $lc_letters .
+        $uc_letters .
+        $numbers .
+        $symbols;
 
+    // inizializzazione password (array)
     $password = [];
-    while (count($password) < $length) {
-        $rand_pos = rand(0, count($pswd_chars) - 1);
-        $rand_char = rand(0, strlen($pswd_chars[$rand_pos]) - 1);
-        $curr_char = $pswd_chars[$rand_pos][$rand_char];
-        array_push($password, $curr_char);
+
+    if ($with_repetition) {
+        // password ha lunghezza non piu grande di $length
+        while (count($password) < $length) {
+            // random char tramite metodo rand
+            $rand_char = $pswd_chars[rand(0, strlen($pswd_chars) - 1)];
+            // pusho rand_char in $password
+            array_push($password, $rand_char);
+        }
+    } else {
+        while (count($password) < $length) {
+            $rand_char = $pswd_chars[rand(0, strlen($pswd_chars) - 1)];
+            // se NO ripetizioni, tolgo da $pswd_chars il char appena assegnato
+            $pswd_chars = str_replace($rand_char, "", $pswd_chars);
+            array_push($password, $rand_char);
+        }
     }
 
-
-    return implode($password);
+    return implode($password);  // array to string
 }
--- index.php
+++ index.php
@@ -3,7 +3,11 @@ require_once './functions.php';
 session_start();
 
 if (isset($_GET['pswd-len']) && is_numeric($_GET['pswd-len']) && (int) $_GET['pswd-len'] > 0 && (int) $_GET['pswd-len'] <= 100) {
-    $_SESSION['password'] = generatePassword((int) $_GET['pswd-len']);
+    $with_repetition = true;
+    if (isset($_GET['repetition']) && $_GET['repetition'] == "no"){
+        $with_repetition = false;
+    }
+    $_SESSION['password'] = generatePassword((int) $_GET['pswd-len'], $with_repetition);
 
     header('Location: ./result.php');
 }
@@ -24,23 +28,36 @@ if (isset($_GET['pswd-len']) && is_numeric($_GET['pswd-len']) && (int) $_GET['ps
 <body class="min-h-screen bg-[#001731]">
     <h1 class="text-4xl mt-8 mb-4 text-center text-slate-400 font-semibold capitalize">strong password generator</h1>
     <h2 class="text-3xl text-center text-white font-semibold">Genera una password sicura</h2>
-    <main class="max-w-2xl mx-auto">
-
-        <!-- output
-        <div class="my-2 p-4 bg-cyan-100 rounded-md text-cyan-800">
-            <?php echo (isset($_GET['pswd-len']) && is_numeric($_GET['pswd-len'])) ? generatePassword((int) $_GET['pswd-len']) : "Nessun parametro valido inserito" ?>
-        </div> -->
+    <main>
 
         <!-- form -->
-        <form method="GET" class="bg-white rounded-md p-4 my-2">
+        <form method="GET" class="bg-white rounded-md p-4 my-2 max-w-lg mx-auto space-y-4">
             <div class="flex justify-between items-center">
                 <label for="pswd-len">Lunghezza password:</label>
                 <input id="pswd-len" name="pswd-len" class="p-2 rounded-sm border border-slate-400" type="number"
                     min="1" max="100">
             </div>
+            <div class="flex justify-between">
+                <p>Consenti ripetizioni di uno o più caratteri:</p>
+                <div class="flex flex-col">
+                    <div class="flex items-center gap-1">
+                        <input id="repetition-yes" name="repetition" type="radio" value="yes" checked>
+                        <label for="repetition-yes">Sì</label>
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <input id="repetition-no" name="repetition" type="radio" value="no">
+                        <label for="repetition-no">No</label>
+                    </div>
+                </div>
+            </div>
 
-            <button class="py-1 px-4 rounded-sm bg-blue-500 text-white cursor-pointer hover:bg-blue-600 "
-                type="submit">Invia</button>
+            <div>
+                <button class="py-1 px-4 rounded-sm bg-blue-500 text-white cursor-pointer hover:bg-blue-600"
+                    type="submit">Invia</button>
+                <button class="py-1 px-4 rounded-sm bg-zinc-500 text-white cursor-pointer hover:bg-zinc-600"
+                    type="reset">
+                    Annulla</button>
+            </div>
         </form>
     </main>
 
