642f21490815ceda2f9e8b5240bd0bde7dc078cd
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-03-13 18:15:42 +0100

    milestone2

--- index.php
+++ index.php
@@ -1,31 +1,4 @@
-<?php
-function generatePassword($length): string
-{
-    if ($length <= 0) return "";
-    $lc_letters = 'abcdefghijklmnopqrstuvwxyz';
-    $uc_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $numbers = '0123456789';
-    $symbols = '!$%&?@#+';
-
-    $pswd_chars = [
-        $lc_letters,
-        $uc_letters,
-        $numbers,
-        $symbols
-    ];
-
-    $password = [];
-    while (count($password) < $length) {
-        $rand_pos = rand(0, count($pswd_chars) - 1);
-        $rand_char = rand(0, strlen($pswd_chars[$rand_pos]) - 1);
-        $curr_char = $pswd_chars[$rand_pos][$rand_char];
-        array_push($password, $curr_char);
-    }
-
-
-    return implode($password);
-}
-?>
+<?php require_once './functions.php' ?>
 
 <!DOCTYPE html>
 <html lang="en">
